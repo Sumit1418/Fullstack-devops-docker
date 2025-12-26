99168808d5432da2578f3f8931307f86640ce78c
Sumit Shah <shahsumitshah707@gmail.com>
2025-12-26 18:28:44 +0530

    CORS removal

--- backend/src/main/java/backend/Controller/HomeController.java
+++ backend/src/main/java/backend/Controller/HomeController.java
@@ -6,7 +6,6 @@ import backend.Entity.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class HomeController {
 
     @Autowired
